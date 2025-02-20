b56615800e32a49da0d4f7528b745f5ec5701ac4
Utkarsh Bhatt <ubhatt2004@gmail.com>
2025-02-21 00:14:50 +0530

    Feature: Add nodemon configuration and update dependencies for improved development experience

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:thoughtflow/Screens/homeScreen.dart';
 import 'package:thoughtflow/Screens/loginScreen.dart';
+import 'package:thoughtflow/Screens/post.dart';
 import 'package:thoughtflow/Screens/signupScreen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:thoughtflow/Screens/Splashscreen.dart';
+import 'package:thoughtflow/provider/provider.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
@@ -14,7 +17,14 @@ void main() async {
     anonKey: dotenv.env['SUPABASE_URL'] ?? '',
   );
 
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+      ],
+      child: const MyApp(),
+    )
+    );
 }
 
 class MyApp extends StatelessWidget {
@@ -32,6 +42,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/splash',
       routes: {
         '/login': (context) => const Loginscreen(),
+        '/post': (context) => const Post(),
         '/home': (context) => const Homescreen(),
         '/signup': (context) => const Signupscreen(),
         '/splash': (context) => const Splashscreen(),
